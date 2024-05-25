4eab3f0956478374fb0c2ba155b93a45001a29f6
Ainsley <hello@ainsley.dev>
2024-05-25 12:17:02 +0100

    chore: Updating build scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,18 @@ import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
 export default [
-	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+		ignores: [
+			'node_modules/*',
+			'templates/'
+		]
+	},
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
 ];
