d89236f7cab47c018dedb966587d1ca0f719b36d
Ainsley Clark <hello@ainsley.dev>
2024-08-03 17:21:19 +0100

    feat: Static image rendering

--- pkg/adapters/static/image.go
+++ pkg/adapters/static/image.go
@@ -1,17 +1,19 @@
 package static
 
 import (
+	"fmt"
 	"github.com/ainsleydev/webkit/pkg/markup"
+	"image"
+	"io"
 	"log/slog"
 	"os"
-	"path"
 	"path/filepath"
 	"strings"
 )
 
 type Image string
 
-const ImageDistPath = "/dist"
+const ImageDistPath = "dist"
 
 // NewImage creates a new image type.
 func NewImage(s string) Image {
@@ -36,18 +38,59 @@ func (i Image) ImageMarkup() markup.ImageProps {
 // PictureMarkup implements the markup.PictureProvider interface and transforms the static image
 // into a markup.PictureProps type ready for rendering a <picture> the DOM.
 func (i Image) PictureMarkup() markup.PictureProps {
+	m := markup.PictureProps{
+		URL:     i.String(),
+		Sources: make([]markup.ImageProps, 0, 10),
+	}
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		slog.Error("Error getting current working directory: " + err.Error())
+		return m
+	}
+
+	for _, path := range i.ImagePaths() {
+		path = filepath.Join(pwd, path)
+		fmt.Println(path)
+
+		// Check if the image file exists
+		if _, err := os.Stat(path); err == nil {
+			m.Sources = append(m.Sources, path.Join(httpPath, imagePath))
+		}
+	}
+
 	return markup.PictureProps{}
 }
 
 // RootPath returns the path relative to the application directory structure.
 // I.e. transforms: /assets/images/hello.jpg to /dist/images.hello.jpg
 func (i Image) RootPath() string {
-	parts := strings.Split(i.String(), "/")
+	parts := strings.Split(strings.TrimPrefix(i.String(), "/"), "/")
 	if len(parts) > 1 {
 		parts[0] = ImageDistPath
 		return "/" + strings.Join(parts, "/")
 	}
 	return i.String()
+}
+
+func (i Image) ImagePaths() []string {
+	img := i.String()
+	baseName := removeFileExtension(filepath.Base(img))
+	dir := filepath.Dir(i.RootPath())
+
+	var s []string
+
+	for _, size := range imageSizes {
+		for _, ext := range append(imageExtensions, filepath.Ext(img)) {
+			s = append(s, filepath.Join(dir, baseName+"-"+size+ext))
+		}
+	}
+
+	for _, ext := range imageExtensions {
+		s = append(s, filepath.Join(dir, baseName+ext))
+	}
+
+	return s
 }
 
 // removeFileExtension removes the file extension from a given filename.
@@ -56,47 +99,33 @@ func removeFileExtension(fileName string) string {
 	return strings.TrimSuffix(fileName, ext)
 }
 
+type imageProperties struct {
+	Width  int
+	Height int
+}
+
+func getProperties(r io.Reader) imageProperties {
+	img, _, err := image.Decode(r)
+	if err != nil {
+		slog.Error(err.Error())
+	}
+
+	bounds := img.Bounds()
+	return imageProperties{
+		Width:  bounds.Max.X,
+		Height: bounds.Max.Y,
+	}
+}
+
 var (
-	sizes = []string{
+	imageSizes = []string{
 		"thumbnail",
 		"mobile",
 		"tablet",
-		"",
+		"desktop",
+	}
+	imageExtensions = []string{
+		".avif",
+		".webp",
 	}
 )
-
-// getImages returns a list of image files associated with the original file in the same directory.
-func getImages(original string) []string {
-	pwd, err := os.Getwd()
-	if err != nil {
-		slog.Error("Error getting current working directory: " + err.Error())
-		return nil
-	}
-
-	// Adjust the base directory path for checking and appending images
-	baseName := removeFileExtension(filepath.Base(original))
-	httpPath := filepath.Dir(original)
-	osPath := strings.ReplaceAll(httpPath, "/assets/", "/dist/")
-
-	extensions := []string{".avif", ".webp", filepath.Ext(original)}
-
-	var images []string
-
-	for _, size := range sizes {
-		for _, ext := range extensions {
-			var imagePath string
-			if size == "" {
-				imagePath = filepath.Join(baseName + ext)
-			} else {
-				imagePath = filepath.Join(baseName + "-" + size + ext)
-			}
-
-			// Check if the image file exists
-			if _, err := os.Stat(filepath.Join(pwd, osPath, imagePath)); err == nil {
-				images = append(images, path.Join(httpPath, imagePath))
-			}
-		}
-	}
-
-	return images
-}
--- pkg/adapters/static/image_test.go
+++ pkg/adapters/static/image_test.go
@@ -20,5 +20,28 @@ func TestImage_PictureMarkup(t *testing.T) {
 }
 
 func TestImage_RootPath(t *testing.T) {
+	input := Image("/assets/images/image.jpg")
+	input.PictureMarkup()
+}
 
+func TestImage_ImagePaths(t *testing.T) {
+	input := Image("/assets/images/image.jpg")
+	want := []string{
+		"/dist/images/image-thumbnail.avif",
+		"/dist/images/image-thumbnail.webp",
+		"/dist/images/image-thumbnail.jpg",
+		"/dist/images/image-mobile.avif",
+		"/dist/images/image-mobile.webp",
+		"/dist/images/image-mobile.jpg",
+		"/dist/images/image-tablet.avif",
+		"/dist/images/image-tablet.webp",
+		"/dist/images/image-tablet.jpg",
+		"/dist/images/image-desktop.avif",
+		"/dist/images/image-desktop.webp",
+		"/dist/images/image-desktop.jpg",
+		"/dist/images/image.avif",
+		"/dist/images/image.webp",
+	}
+	got := input.ImagePaths()
+	assert.Equal(t, want, got)
 }
