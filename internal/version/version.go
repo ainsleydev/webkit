af81ca01cd299ec87c3460f3abb25be585da0523
Ainsley Clark <hello@ainsley.dev>
2025-10-20 16:35:26 +0100

    feat: Go releaser

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,5 +1,20 @@
 package version
 
-// Version is the current version number that Webkit is
-// running on.
-var Version = "0.0.0"
+// Version is the current version number that Webkit is running on.
+// This value is injected by GoReleaser during builds via ldflags.
+var Version = "dev"
+
+// BuildInfo contains additional build metadata injected by GoReleaser.
+var (
+	Commit  = "none"
+	Date    = "unknown"
+	BuiltBy = "unknown"
+)
+
+// Info returns a formatted string with all version information.
+func Info() string {
+	return "WebKit " + Version + "\n" +
+		"  Commit:   " + Commit + "\n" +
+		"  Built:    " + Date + "\n" +
+		"  Built by: " + BuiltBy
+}
