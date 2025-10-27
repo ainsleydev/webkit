af4768f418c8767db8ab26a62347152795d1ab21
Ainsley Clark <34712954+ainsleyclark@users.noreply.github.com>
2025-10-27 09:03:40 +0000

    chore: Rotating age key (#59)

--- internal/secrets/secrets.go
+++ internal/secrets/secrets.go
@@ -7,7 +7,7 @@ import (
 )
 
 // AgePublicKey is the public key for encrypting SOPS files.
-const AgePublicKey = "age1ql3z7hjy54pw3hyww5ayyfg7zqgvc7w3j2elw8zmrj2kg5sfn9aqmcac8p"
+const AgePublicKey = "age1mcl448l48v0e4t5ljek8htn5s07amz5zwr54hkq5qpawgapc9dpstnhksq"
 
 // FilePath defines the path where SOPS encrypted YAML files
 // reside in the Webkit app. Needs a base path prepended.
