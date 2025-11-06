b5f322aea75f003805af303544ef952afd7a38b1
Ainsley <hello@ainsley.dev>
2025-11-06 07:13:18 +0000

    refactor: Adding icmp

--- platform/terraform/providers/digital_ocean/droplet/main.tf
+++ platform/terraform/providers/digital_ocean/droplet/main.tf
@@ -62,6 +62,11 @@ resource "digitalocean_firewall" "this" {
     source_addresses = ["0.0.0.0/0", "::/0"]
   }
 
+  inbound_rule {
+    protocol         = "icmp"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "all"
