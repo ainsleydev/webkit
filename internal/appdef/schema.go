c6b8aa44a165430bf0b89fd81a7dcac8d021ae6d
Ainsley <hello@ainsley.dev>
2025-11-09 19:44:57 +0000

    refactor: Schema changes

--- internal/appdef/schema.go
+++ internal/appdef/schema.go
@@ -6,7 +6,6 @@ import (
 	"github.com/swaggest/jsonschema-go"
 )
 
-//go:generate go run ../../cmd/schema/main.go --output=./schema.json
 //go:generate go run ../../cmd/schema/main.go --output=../templates/schema.json
 
 // GenerateSchema generates a JSON schema for the Definition type.
--- internal/cmd/files/schema.go
+++ internal/cmd/files/schema.go
@@ -5,17 +5,14 @@ import (
 	"path/filepath"
 
 	"github.com/ainsleydev/webkit/internal/cmdtools"
-	"github.com/ainsleydev/webkit/internal/manifest"
-	"github.com/ainsleydev/webkit/internal/scaffold"
 	"github.com/ainsleydev/webkit/internal/templates"
 )
 
 // Schema copies the JSON schema to the .webkit folder.
 func Schema(_ context.Context, input cmdtools.CommandInput) error {
-	return input.Generator().Template(filepath.Join(".webkit", "schema.json"),
-		templates.MustLoadTemplate("schema.json"),
-		nil,
-		scaffold.WithTracking(manifest.SourceProject()),
-		scaffold.WithoutNotice(),
+	return input.Generator().CopyFromEmbed(
+		templates.Embed,
+		"schema.json",
+		filepath.Join(".webkit", "schema.json"),
 	)
 }
