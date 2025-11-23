b9fcda7478ab8b86cc51eed789efd8319418f2b8
Ainsley <hello@ainsley.dev>
2025-11-23 13:54:44 +0000

    fix: Max redirects

--- platform/terraform/modules/monitoring/variables.tf
+++ platform/terraform/modules/monitoring/variables.tf
@@ -22,6 +22,7 @@ variable "monitors" {
     method        = optional(string)
     domain        = optional(string) # For DNS monitors.
     interval      = number           # Interval in seconds between checks.
+    max_redirects = optional(number) # For HTTP monitors, max redirects to follow (default 0).
     variable_name = optional(string) # Pre-computed GitHub variable name (e.g., PROD_DB_BACKUP_PING_URL).
   }))
   default = []
