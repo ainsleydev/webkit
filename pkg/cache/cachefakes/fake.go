d02044091d75d5d56e0271c33796b1a310da0b03
Ainsley <hello@ainsley.dev>
2025-11-28 09:49:04 +0000

    chore: Updating version to v0.9.2

--- pkg/cache/cachefakes/fake.go
+++ pkg/cache/cachefakes/fake.go
@@ -21,7 +21,6 @@ import (
 type MockStore struct {
 	ctrl     *gomock.Controller
 	recorder *MockStoreMockRecorder
-	isgomock struct{}
 }
 
 // MockStoreMockRecorder is the mock recorder for MockStore.
